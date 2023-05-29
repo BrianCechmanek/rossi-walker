2125e5627defcd8e16c8f29d4de9ac6c41e71406
BrianCechmanek <bcechmanek@gmail.com>
2023-05-29 23:56:15 +0100

    initial commit, one page app

---
+++ README.md
@@ -0,0 +1,18 @@
+# rossi_walker
+
+A personal project for tracking the walking of my dog. This app should not be used by anyone (and once I enable gmail login, won't even be an option except for me to make mistakes with). 
+
+**Rest TBD**
+
+## Getting Started
+
+This project is a starting point for a Flutter application.
+
+A few resources to get you started if this is your first Flutter project:
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,346 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(RossiApp());
+}
+
+class RossiApp extends StatelessWidget {
+  const RossiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => RossiAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(89, 19, 192, 189)),
+        ),
+        home: SubmitPage(),
+      ),
+    );
+  }
+}
+
+class RossiAppState extends ChangeNotifier {
+  var walker = "None";
+  var time = "None";
+
+  var currentPair = WordPair.random();
+  var favourites = <WordPair>[];
+
+  void getNext() {
+    currentPair = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleWalker() {
+    if (walker == "MB") {
+      print("toggled MB");
+    } else if (walker == "B") {
+      print("toggled B");
+    } else {
+      print("toggled {$walker}");
+    }
+    notifyListeners();
+  }
+
+  void toggleFavourite() {
+    if (favourites.contains(currentPair)) {
+      favourites.remove(currentPair);
+    } else {
+      favourites.add(currentPair);
+    }
+    notifyListeners();
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selectedIndex = 0;
+  
+  var walker = "None";
+
+  @override 
+  Widget build(BuildContext context) {
+    Widget page;
+    switch (selectedIndex) {
+      case 0:
+        page = SubmitPage();
+        break;
+      case 1: 
+        page = const Placeholder(); //calendar();
+        break;
+      case 2:
+        page = const Placeholder(); //stats
+        break;
+      case 3:
+        page = const Placeholder(); //edit
+        break;
+      default: 
+        throw UnimplementedError('No widget for $selectedIndex');
+    }
+
+    return Scaffold(
+      body: Row(
+        children: [
+          SafeArea(
+            child: NavigationRail(
+              extended: false,
+              destinations: [
+                NavigationRailDestination(
+                  icon: Icon(Icons.directions_walk ),
+                  label: Text('Submit Walk'),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.calendar_month_outlined), 
+                    label: Text('Calendar'),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.add_chart_outlined),
+                    label: Text('Stats'),
+                ),
+                NavigationRailDestination(
+                    icon: Icon(Icons.mode_edit_outline_outlined), 
+                    label: Text('Edit')
+                ),
+              ],
+              selectedIndex: selectedIndex,
+              onDestinationSelected: (value) {
+                setState(() {selectedIndex = value;});
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: page,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SubmitPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<RossiAppState>();
+
+    // IconData icon;
+    // if (appState.favourites.contains(pair)) {
+    //   icon = Icons.favorite;
+    // } else {
+    //   icon = Icons.favorite_border;
+    // }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("MB"),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("B"),
+              ),
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("A"),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("L"),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("D"),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleWalker();
+                },
+                icon: Icon(Icons.check_box_outline_blank),
+                label: Text("E"),
+              ),
+            ],            
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+class GeneratorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<RossiAppState>();
+    var pair = appState.currentPair;
+
+    IconData icon;
+    if (appState.favourites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BigCard(pair: pair),
+          SizedBox(height: 10),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  appState.toggleFavourite();
+                },
+                icon: Icon(icon),
+                label: Text('Like'),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  appState.getNext();
+                },
+                child: Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class FavouritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<RossiAppState>();
+    var favourites = appState.favourites;
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onSecondary,
+      fontSize: 20,
+    );
+
+    if (favourites.isEmpty) {
+      return Center(
+        child: Card(
+            color: theme.colorScheme.secondary,
+            elevation: 150,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                style: style,
+                'Add Favourites to see them here.'
+              ),
+            ),
+          ),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text(
+            'You have ${favourites.length} fav\'s:' 
+          ),
+        ),
+        for (var pair in favourites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+    // return ListView.builder(
+    //   padding: const EdgeInsets.all(9),
+    //   itemCount: favourites.length,
+    //   itemBuilder: (BuildContext context, int index) {
+    //     return Container(
+    //       height: 50,
+    //       color: Theme.of(context).colorScheme.secondaryContainer,
+    //       child: Center(child: Text('Entry ${favourites[index]}')),
+    //     );
+    //   }
+    // );
+  }
+}
+
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onSecondary,
+      fontSize: 20,
+    );
+
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 150,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asPascalCase, 
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}"
+        ),
+      ),
+    );
+  }
+}
