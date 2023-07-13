f6eca4700dce1452a092309fe28baa16709203f1
BrianCechmanek <bcechmanek@gmail.com>
2023-07-13 01:36:48 +0100

    kicking off firebase deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async {
+  // initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const RossiApp());
 }
 
